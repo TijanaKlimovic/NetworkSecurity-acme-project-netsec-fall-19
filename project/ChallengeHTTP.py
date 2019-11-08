729106791e1df9c00e5072d4e1edc2916465ef44
TijanaKlimovic <tijana.klimovic.tk@gmail.com>
2019-11-08 21:13:03 +0100

    work7

--- project/ChallengeHTTP.py
+++ project/ChallengeHTTP.py
@@ -32,8 +32,8 @@ def main():
     parser.add_argument('record')
     args = vars(parser.parse_args())
     #print('THE RECORD IS:' ,args.get('record'))
-    #app.config['SERVER_NAME'] = args.get('record') + ':5002'
-    app.run(host=args.get('record'), port=5002)
+    app.config['SERVER_NAME'] = args.get('record') + ':5002'
+    #app.run(host=args.get('record'), port=5002)
     #app.run()
 
 if __name__ == "__main__":
